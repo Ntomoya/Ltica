0224dd24b9111735d7b94c7782d3d0615f005e68
ntomoya <ntomoya1010@gmail.com>
2016-05-27 23:20:09 +0900

    serial test

--- SerialTest.java
+++ SerialTest.java
@@ -1,12 +1,10 @@
 import com.pi4j.io.serial.*;
 
 public class SerialTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // create an instance of the serial communications class
         final Serial serial = SerialFactory.createInstance();
-
-        serial.open(Serial.DEFAULT_COM_PORT, 115200);
 
         // たぶん受信処理
         serial.addListener(new SerialDataListener() {
@@ -17,9 +15,18 @@ public class SerialTest {
                 }
             });
 
-        // 送信処理
         try {
-            serial.writeln("Hello world!");
-        } catch
+            serial.open(Serial.DEFAULT_COM_PORT, 115200);
+            // 送信処理
+            try {
+                serial.writeln("Hello world!");
+            } catch(IllegalStateException ex) {
+                ex.printStackTrace();
+            }
+        } catch(SerialPortException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+        serial.shutdown();
     }
 }
